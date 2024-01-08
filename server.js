0e98d8dcafd1737ec7d98017a7225775e5c5f712
Samir B <smdae99@gmail.com>
2024-01-08 14:03:09 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,3 @@
+// server.js
+require('dotenv').config() // this line is mandatory when you want to read variables from the '.env' file 
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
